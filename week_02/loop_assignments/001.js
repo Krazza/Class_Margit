aef22e1f6f07d362b4bc2a8adb3370b521369746
student <student@BC-minimac019s-Mac-mini.local>
2022-08-17 14:49:58 +0300

    update (loop assignments)

---
+++ week_02/loop_assignments/001.js
@@ -0,0 +1,14 @@
+// for(let i = 1; i <= 100; i+=2)
+// {
+//     console.log(i);
+// }
+
+// let s = "";
+
+// for(let a = 2, i = 98; a < 100, i > 2; a += 2, i -= 2)
+// {
+//     s = s + a + " ";
+//     s = s + i + " ";
+// }
+
+// console.log(s);
