38f46ada02d80d2eafad8931882b9533f8fd0bb4
Muondo Vladislav <s2200741@BC-C02J64ATQ6LR-MacBook-Air.local>
2022-08-24 15:41:52 +0300

    gas calculator, finished

--- week_03/gasonline/main.js
+++ week_03/gasonline/main.js
@@ -1,15 +1,14 @@
 function OnClick()
 {
-    console.log("hello there");
-}
-
-const calculate = () => 
-{
     const price = Number(document.getElementById('price').value);
     const money = +document.querySelector('#money').value;
     const answer = document.querySelector('#answer');
 
     const amount = Math.floor(money/price);
     answer.textContent = amount;
-}
 
+    if(amount < 10)
+        answer.textContent = answer.textContent + " litres || Ups, you have to stay here.";
+    else if(amount > 10)
+        answer.textContent = answer.textContent + " litres || Good, you can escape now.";
+}
