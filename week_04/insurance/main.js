2806622a56ec323547f6f9b06fed5b295296ce71
Muondo Vladislav <s2200741@BCC02J6TQ6LRAir.lan>
2022-08-30 20:34:15 +0300

    pizza class practice, insurance homework

---
+++ week_04/insurance/main.js
@@ -0,0 +1,46 @@
+function HandleInput()
+{
+    let userNameInput = document.querySelector("#userName").value;
+    let userAge = document.querySelector("#userAge").value;
+    
+    let userConditionsArray = [];
+    let userConditionsInput = document.querySelectorAll('input[name=conditionsInput]:checked');
+    userConditionsArray = Array.from(userConditionsInput).map(item => item.value);
+
+    let userHabitsArray = [];
+    let userHabitsInput = document.querySelectorAll('input[name=habitsInput]:checked');
+    userHabitsArray = Array.from(userHabitsInput).map(item => item.value);
+
+    let output = document.querySelector("#results");
+
+    let price = 500;
+
+    if(userAge >= 18 && userAge <= 25)
+        price = price + (price * 0.1);
+    else if(userAge >= 26 && userAge <= 35)
+        price = price + (price * 0.3)
+    else if(userAge >= 36 && userAge <= 45)
+        price = price + (price * 0.6)
+    else if(userAge >= 46 && userAge <= 55)
+        price = price * 2;
+    else if(userAge >= 56 && userAge <= 65)
+        price = price + (price * 1.5);
+    else if(userAge >= 66)
+        price = price + (price * 2.1);
+
+
+    userConditionsArray.forEach(function()
+    {
+        price = price + (price * 0.01);
+    });
+
+    for(let i = 0; i < userHabitsArray.length; i++)
+    {
+        if(userHabitsArray[i] == "bad")
+            price = price + (price * 0.05);
+        else if(userHabitsArray[i] == "good")
+            price = price - (price * 0.05);
+    }
+
+    output.innerHTML = `Here is the price of your insurance: ${Math.floor(price)}€, ${userNameInput}.`;
+}
--- week_04/pizza/main.js
+++ week_04/pizza/main.js
@@ -1,2 +1,45 @@
-let delivery = document.querySelector("DMethod");
+function Debug()
+{
+    let userNameTxtInp = document.querySelector("#userName");
+    let pizzaSizeRadInp = document.querySelector('input[name=Pzsize]:checked').value;
+    
+    let toppingsCheckInp = document.querySelectorAll('input[name=tops]:checked');
+    let toppingsCheckBxInpt = [];
+    toppingsCheckBxInpt = Array.from(toppingsCheckInp).map(item => item.value);
 
+    let selectDeliveryInp = document.querySelector('#DMethod');
+    let selectedDlvrOption = selectDeliveryInp.options[selectDeliveryInp.selectedIndex];
+
+    let price = 0;
+
+    switch(pizzaSizeRadInp)
+    {
+        case "2":
+            price += 7.5;
+            break;
+        case "4":
+            price += 10.5;
+            break;
+        case "6":
+            price += 12.5;
+            break;
+        case "8":
+            price += 15.5;
+            break;
+        default:
+            break;
+    }
+
+    if(toppingsCheckBxInpt.length > 4)
+        price += (toppingsCheckBxInpt.length - 4) * 0.5;
+
+    if(selectedDlvrOption.value == "delivery")
+        price += 5;
+    
+
+    console.log(userNameTxtInp.value);
+    console.log(pizzaSizeRadInp);
+    console.log(toppingsCheckBxInpt);
+    console.log(selectedDlvrOption.value);
+    console.log(price);
+}
